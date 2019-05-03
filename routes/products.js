02a34c1ce63d9d7957f6b645ededb04495156b3a
kmietotwy <kmietotwy@gmail.com>
2019-05-02 19:51:24 -1000

    updated status codes for products

--- routes/products.js
+++ routes/products.js
@@ -6,13 +6,10 @@ router.route('/').get((req, res) => {
   knex
     .raw('select * from products')
     .then((productObject) => {
-
-      res.status(200);
-      res.send(productObject.rows);
+      return res.status(200).send(productObject.rows);
     })
     .catch((err) => {
-      res.status(400);
-      res.send(err);
+      return res.status(500).send("{ 'message' : 'Database error' }");
     });
 });
 
@@ -23,33 +20,25 @@ router
       .raw('select * from products where id = ?', [req.params.product_id])
       .then((productObject) => {
         if (productObject.rows.length === 0) {
-          throw '{ "message": "Product not found" }';
+          return res.status(404).send('{ "message": "Product not found" }');
         }
 
-        res.status(200);
-        res.send(productObject.rows);
+        return res.status(200).send(productObject.rows);
       })
       .catch((err) => {
-        res.status(400);
-        res.send(err);
+        return res.status(500).send('{ "message": "Database error" }');
       });
   })
   .put((req, res) => {
-    if(!req.body.title ||
-      !req.body.description ||
-      !req.body.inventory ||
-      !req.body.price){
-
-        res.status(400);
-        res.send('{ "message": "Fill out all fields! }');
-        return;
-      }
+    if (!req.body.title || !req.body.description || !req.body.inventory || !req.body.price) {
+      return res.status(400).send('{ "message": "Fill out all fields! }');
+    }
 
     knex
       .raw('select * from products where id = ?', [req.params.product_id])
       .then((productObject) => {
         if (productObject.rows.length === 0) {
-          throw `{ "message": "Product id: ${req.params.product_id} not found" }`;
+          return res.status(404).send(`{ "message": "Product id: ${req.params.product_id} not found" }`);
         }
 
         return knex.raw(
@@ -58,12 +47,10 @@ router
         );
       })
       .then(function(updatedProduct) {
-        res.status(200);
-        res.send(`{ "message": "Product: ${req.params.product_id} has been updated" }`);
+        return res.status(200).send(`{ "message": "Product: ${req.params.product_id} has been updated" }`);
       })
       .catch((err) => {
-        res.status(400);
-        res.send(err);
+        return res.status(400).send('{ "message": "Database error" }');
       });
   })
   .delete((req, res) => {
@@ -71,18 +58,16 @@ router
       .raw('select * from products where id = ?', [req.params.product_id])
       .then((productObject) => {
         if (productObject.rows.length === 0) {
-          throw `{ "message": "Product id: ${req.params.product_id} not found" }`;
+          return res.status(404).send(`{ "message": "Product id: ${req.params.product_id} not found" }`);
         }
 
         return knex.raw('delete from products where id = ? returning *', [req.params.product_id]);
       })
       .then(function(deletedProduct) {
-        res.status(200);
-        res.send(`{ "message": "Product: ${req.params.product_id} successfully deleted" }`);
+        return res.status(200).send(`{ "message": "Product: ${req.params.product_id} successfully deleted" }`);
       })
       .catch((err) => {
-        res.status(400);
-        res.send(err);
+        return res.status(500).send('{ "message": "Database error" }');
       });
   });
 
@@ -91,13 +76,13 @@ router.route('/new').post((req, res) => {
     .raw('select * from products where title = ?', [req.body.title])
     .then(function(productObject) {
       if (productObject.rows.length !== 0) {
-        throw '{ "message": "Product already exists" }';
+        return res.status(400).send('{ "message": "Product already exists" }');
       }
       return req.body;
     })
     .then(function(newProduct) {
       if (!newProduct.title || !newProduct.description || !newProduct.inventory || !newProduct.price) {
-        throw '{ "message": "Must POST all product fields" }';
+        return res.status(400).send('{ "message": "Must POST all product fields" }');
       }
 
       return knex.raw('insert into products (title, description, inventory, price) values (?, ?, ?, ?) returning *', [
@@ -108,12 +93,10 @@ router.route('/new').post((req, res) => {
       ]);
     })
     .then(function(newProductDetail) {
-      res.status(200);
-      res.send(newProductDetail.rows);
+      return res.status(200).send(newProductDetail.rows);
     })
     .catch((err) => {
-      res.status(400);
-      res.send(err);
+      return res.status(500).send('{ "message": "Database error" }');
     });
 });
 
