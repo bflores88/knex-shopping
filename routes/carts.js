ea761a14b641b8363416622b5ed689e804065f6e
kmietotwy <kmietotwy@gmail.com>
2019-05-02 15:33:12 -1000

    carts get product per user id working

--- routes/carts.js
+++ routes/carts.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../database');
 
-router.route('/carts')
-  .get((req, res) => {
+router.route('/:user_id').get((req, res) => {
+  knex
+    .raw('select * from users where id = ?', [req.params.user_id])
+    .then((userObject) => {
+      if (userObject.rows.length === 0) {
+        throw '{ "message": "This user does not exist!"}';
+      }
 
-  });
+      return req.params.user_id;
+    })
+    .then((userID) => {
+      return knex.raw(
+        'select * from products inner join carts on products.id = carts.product_id where carts.user_id = ?',
+        [userID],
+      );
+    })
+    .then((productInfo) => {
+      if (productInfo.rows.length === 0) {
+        throw '{ "message": "This user has nothing in the cart"}';
+      }
+
+      res.send(productInfo.rows);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
