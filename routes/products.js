72f3531801e905e62747215f83c124dcd8b59005
kmietotwy <kmietotwy@gmail.com>
2019-05-02 14:46:47 -1000

    get products working

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../database');
 
-router.route('/products')
-  .get((req, res) => {
+router.route('/').get((req, res) => {
+  knex
+    .raw('select * from products')
+    .then((productObject) => {
+      if (productObject.rows.length === 0) {
+        throw '{ "message": "No products found!" }';
+      }
 
-  });
+      res.send(productObject.rows);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database');
 
-router.route('/').get((req, res) => {
-  res.send('what');
-});
-
 router
   .route('/:user_id')
   .get((req, res) => {
