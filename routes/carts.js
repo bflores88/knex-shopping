4f2ab53c86d38b24295235ab2245b646b81aaad4
kmietotwy <kmietotwy@gmail.com>
2019-05-02 15:47:37 -1000

    carts post working and checks users and products before updating carts

--- routes/carts.js
+++ routes/carts.js
@@ -30,4 +30,38 @@ router.route('/:user_id').get((req, res) => {
     });
 });
 
+router.route('/:user_id/:product_id').post((req, res) => {
+  knex
+    .raw('select * from users where id = ?', [req.params.user_id])
+    .then((userObject) => {
+      if (userObject.rows.length === 0) {
+        throw '{ "message": "This user does not exist!"}';
+      }
+
+      return req.params;
+    })
+    .then((params) => {
+      return knex.raw('select * from products where id = ?', [params.product_id]);
+    })
+    .then((productObject) => {
+      if (productObject.rows.length === 0) {
+        throw '{ "message": "This product does not exist!"}';
+      }
+
+      return req.params;
+    })
+    .then((params) => {
+      return knex.raw('insert into carts (user_id, product_id) values (?, ?) returning *', [
+        params.user_id,
+        params.product_id,
+      ]);
+    })
+    .then((cartObject) => {
+      res.send('{ "success": true }');
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
 module.exports = router;
