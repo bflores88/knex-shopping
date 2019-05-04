21fe79a4fbefd663dbbb01122cbdfce7cc8b5e77
kmietotwy <kmietotwy@gmail.com>
2019-05-03 15:28:47 -1000

    refactored users to nest returned promises

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router
         if (userObject.rows.length === 0) {
           return res.status(404).send('{ "message": "User not found!" }');
         }
-        
+
         return res.status(200).send(userObject.rows);
       })
       .catch((err) => {
@@ -28,10 +28,9 @@ router
 
         let userDetail = userObject.rows[0];
 
-        return knex.raw('delete from users where id = ? returning *', [userDetail.id]);
-      })
-      .then(function(deleteReturn) {
-        return res.status(200).send(`{ "message": "User id: ${req.params.user_id} successfully deleted" }`);
+        return knex.raw('delete from users where id = ? returning *', [userDetail.id]).then(function(deleteReturn) {
+          return res.status(200).send(`{ "message": "User id: ${req.params.user_id} successfully deleted" }`);
+        });
       })
       .catch((err) => {
         return res.status(500).send('{ "message": "Database error" }');
@@ -61,11 +60,10 @@ router.route('/login').post((req, res) => {
         return res.status(404).send('{ "message": "User not found" }');
       }
 
-      return userObject.rows[0];
-    })
-    .then(function(userRows) {
-      if (req.body.password !== userRows.password) {
-        throw '{ "message": "Incorrect password" }';
+      let productDetail = userObject.rows[0];
+
+      if (req.body.password !== productDetail.password) {
+        return res.status(400).send('{ "message": "Incorrect password" }');
       }
 
       return res.status(200).send(userRows);
@@ -82,16 +80,12 @@ router.route('/register').post((req, res) => {
       if (userObject.rows.length !== 0) {
         return res.status(400).send('{ "message": "User already exists" }');
       }
-      return req.body;
-    })
-    .then(function(newUser) {
-      return knex.raw('insert into users (email, password) values (?, ?) returning *', [
-        newUser.email,
-        newUser.password,
-      ]);
-    })
-    .then(function(newUserDetail) {
-      return res.status(200).send(newUserDetail.rows);
+      let newUser = req.body;
+      return knex
+        .raw('insert into users (email, password) values (?, ?) returning *', [newUser.email, newUser.password])
+        .then(function(newUserDetail) {
+          return res.status(200).send(newUserDetail.rows);
+        });
     })
     .catch((err) => {
       return res.status(500).send('{ "message": "Database error" }');
