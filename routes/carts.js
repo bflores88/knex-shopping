332a4c8d9fe71062f56172a9490265d4cd29ad28
kmietotwy <kmietotwy@gmail.com>
2019-05-04 15:13:07 -1000

    removed console.log in carts

--- routes/carts.js
+++ routes/carts.js
@@ -15,7 +15,6 @@ router.route('/:user_id').get((req, res) => {
             req.params.user_id
           ])
           .then((productInfo) => {
-            console.log(productInfo);
             if (productInfo.rows.length === 0) {
               return res.status(404).send('{ "message": "This user has nothing in the cart"}');
             }
